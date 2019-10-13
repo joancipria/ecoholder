9bb3aca772eecd9548cef04ece9156aa13fcf4bf
Joan Ciprià <joancipria@gmail.com>
2019-10-13 17:07:45 +0200

    Implemented guardarS02()

--- src/app/core/services/servidorFake.service.ts
+++ src/app/core/services/servidorFake.service.ts
@@ -1,11 +1,28 @@
-import {Injectable} from "@angular/core";
+import { Injectable } from "@angular/core";
+import { HttpParams, HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class ServidorFake {
    constructor(
+      private http: HttpClient
    ) {
-      
+
    }
 
+   guardarSo2(so2) {
+      let payload = new HttpParams()
+         .set('value', so2);
 
+      this.http.post('http://192.168.43.141:3000/api/measure', payload)
+         .subscribe(
+            res => {
+               console.log(res);
+            },
+            err => {
+               if (err){
+                  console.log("Error occured"+err);
+               }
+            }
+         );
+   }
 }
