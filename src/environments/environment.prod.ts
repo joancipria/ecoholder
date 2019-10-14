9db6eb95bac12d4b6b72be0b0d2b8c45e576a02b
Joan Ciprià <joancipria@gmail.com>
2019-10-14 18:51:55 +0200

    Added firebase config to production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAGs7A2TWsI4DBPekp19f9TWz1RA0D-pTI",
+    authDomain: "proyecto3a-fd2f8.firebaseapp.com",
+    databaseURL: "https://proyecto3a-fd2f8.firebaseio.com",
+    projectId: "proyecto3a-fd2f8",
+    storageBucket: "proyecto3a-fd2f8.appspot.com",
+    messagingSenderId: "838602587969",
+    appId: "1:838602587969:web:041b70bab3e9cbe976abc0",
+    measurementId: "G-9M4XGGVJ0F"
+  }
 };
