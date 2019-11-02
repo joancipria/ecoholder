70381d4ad3279706dff1472d065a70cb71425c92
rapercle <rapercle@epsg.upv.es>
2019-11-02 23:59:57 +0100

    grafica home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,38 @@ export class HomePage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.showChart();
+  }
+
+  showChart(){
+    var ctx = (<any>document.getElementById('yudhatp-chart')).getContext('2d');
+    var chart = new Chart(ctx, {
+      type: 'pie', 
+      data: {
+        labels: ["SO2", "CO2", "NOX"],
+        datasets: [{
+          label: "This is chart",
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 163, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 163, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          data: [20, 5, 10],
+          borderWidth:1
+        }]
+      }
+    })
   }
 
 }
