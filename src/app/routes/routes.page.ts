13705bfce90728da158769e59a472737c3a696b7
Joan Ciprià <joancipria@gmail.com>
2019-12-06 21:11:05 +0100

    Implemented show/hide modal

--- src/app/routes/routes.page.ts
+++ src/app/routes/routes.page.ts
@@ -26,10 +26,12 @@ import { Maps } from '../core/services/maps.service';
 export class RoutesPage implements OnInit {
   @ViewChild('map', { static: false }) element;
   @ViewChild('search', { static: false }) elementSearch;
+  @ViewChild('directionsButton', { static: false }) elementDirectionsButton;
   showSelectContaminante: boolean = false;
   @ViewChild('select', { static: false }) select: IonSelect;
   private cuadricula = false;
   private toggleCuadricula = false;
+  private directionsModal: any;
 
   constructor(
     private router: Router,
@@ -49,77 +51,82 @@ export class RoutesPage implements OnInit {
   }
 
   // Wait for dom
-  ionViewWillEnter() {
+  async ionViewWillEnter() {
+    // Crear modal
+    this.directionsModal = await this.modalController.create({
+      component: DirectionsPage,
+      backdropDismiss: false,
+      componentProps: {parentRef: this}
+    });
+    this.directionsModal.style = "display: none;" // Ocultar por defecto
+    this.directionsModal.present();
+
     this.plt.ready().then(() => {
       // Load just once
-        this.maps.inicializarMapa(this.element, this.elementSearch);
+      this.maps.inicializarMapa(this.element, this.elementSearch, this.directionsModal.childNodes[1], this.elementDirectionsButton);
     });
+
   }
 
-// ----------------------------------------------------------------------
-// Evento que se ejecuta al cambiar de contaminante
-// evento onChange -> f() ->
-// ------------------------------------------------------------------------
-  onChangeContaminante($event) {
+  // ----------------------------------------------------------------------
+  // Evento que se ejecuta al cambiar de contaminante
+  // evento onChange -> f() ->
+  // ------------------------------------------------------------------------
+  onChangeContaminante($event) {
 
-        console.log("el sensor seleccionado es: " + $event.target.value);
+    console.log("el sensor seleccionado es: " + $event.target.value);
 
-        const contaminanteSeleccionado =  $event.target.value;
-        switch(contaminanteSeleccionado) {
-          case 'CO2':
-              const g = [
-                'rgba(255, 0, 0, 0)',
-                'rgba(255, 255, 0, 0.9)',
-                'rgba(0, 255, 0, 0.7)',
-                'rgba(173, 255, 47, 0.5)',
-                'rgba(152, 251, 152, 0)',
-                'rgba(152, 251, 152, 0)',
-                'rgba(0, 0, 238, 0.5)',
-                'rgba(186, 85, 211, 0.7)',
-                'rgba(255, 0, 255, 0.9)',
-                'rgba(255, 0, 0, 1)'];
-                this.maps.cambiarGradiente(g);
+    const contaminanteSeleccionado = $event.target.value;
+    switch (contaminanteSeleccionado) {
+      case 'CO2':
+        const g = [
+          'rgba(255, 0, 0, 0)',
+          'rgba(255, 255, 0, 0.9)',
+          'rgba(0, 255, 0, 0.7)',
+          'rgba(173, 255, 47, 0.5)',
+          'rgba(152, 251, 152, 0)',
+          'rgba(152, 251, 152, 0)',
+          'rgba(0, 0, 238, 0.5)',
+          'rgba(186, 85, 211, 0.7)',
+          'rgba(255, 0, 255, 0.9)',
+          'rgba(255, 0, 0, 1)'];
+        this.maps.cambiarGradiente(g);
 
-            this.maps.cambiarRadius(60);
-            break;
-          case 'NOX':
+        this.maps.cambiarRadius(60);
+        break;
+      case 'NOX':
 
 
-      const gradient = [
-        'rgba(0, 255, 255, 0)',
-        'rgba(0, 255, 255, 1)',
-        'rgba(0, 191, 255, 1)',
-        'rgba(0, 127, 255, 1)',
-        'rgba(0, 63, 255, 1)',
-        'rgba(0, 0, 255, 1)',
-        'rgba(0, 0, 223, 1)',
-        'rgba(0, 0, 191, 1)',
-        'rgba(0, 0, 159, 1)',
-        'rgba(0, 0, 127, 1)',
-        'rgba(63, 0, 91, 1)',
-        'rgba(127, 0, 63, 1)',
-        'rgba(191, 0, 31, 1)',
-        'rgba(255, 0, 0, 1)'
-      ];
-            this.maps.cambiarGradiente(gradient);
-            this.maps.cambiarRadius(80);
-            break;
+        const gradient = [
+          'rgba(0, 255, 255, 0)',
+          'rgba(0, 255, 255, 1)',
+          'rgba(0, 191, 255, 1)',
+          'rgba(0, 127, 255, 1)',
+          'rgba(0, 63, 255, 1)',
+          'rgba(0, 0, 255, 1)',
+          'rgba(0, 0, 223, 1)',
+          'rgba(0, 0, 191, 1)',
+          'rgba(0, 0, 159, 1)',
+          'rgba(0, 0, 127, 1)',
+          'rgba(63, 0, 91, 1)',
+          'rgba(127, 0, 63, 1)',
+          'rgba(191, 0, 31, 1)',
+          'rgba(255, 0, 0, 1)'
+        ];
+        this.maps.cambiarGradiente(gradient);
+        this.maps.cambiarRadius(80);
+        break;
 
-          case 'SO2':
-            this.maps.cambiarRadius(20);
+      case 'SO2':
+        this.maps.cambiarRadius(20);
 
-        }
-      }
-
-  calcRoute(destination){
-    this.maps.calcularRuta(destination);
+    }
   }
 
-
-// ----------------------------------------------------------------------
-// Mostrar / Ocultar el select de contaminante
-// -> f() ->
-// ------------------------------------------------------------------------
+  // ----------------------------------------------------------------------
+  // Mostrar / Ocultar el select de contaminante
+  // -> f() ->
+  // ------------------------------------------------------------------------
   toggleSelectorContaminante() {
     this.select.open();
     // this.showSelectContaminante = !this.showSelectContaminante;
@@ -142,14 +149,11 @@ export class RoutesPage implements OnInit {
     this.toggleCuadricula = this.maps.toggleCuadricula();
   }
 
-  public async mostrarDirections() {
-    const modal = await this.modalController.create({
-      component: DirectionsPage
-    });
-    return await modal.present();
+  public mostrarDirections() {
+    this.directionsModal.style="display: flex;";
   }
 
-  public async cerrarDirections(){
-    await this.modalController.dismiss();
+  public cerrarDirections() {
+    this.directionsModal.style="display: none;";
   }
 }
