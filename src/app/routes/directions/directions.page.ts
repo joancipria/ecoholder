86a28ae584cc9a13fe957294334d83e79e17c62a
Joan Ciprià <joancipria@gmail.com>
2019-12-06 20:49:30 +0100

    Added close method to the modal

--- src/app/routes/directions/directions.page.ts
+++ src/app/routes/directions/directions.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavParams} from "@ionic/angular";
 
 @Component({
   selector: 'app-directions',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirectionsPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private navParams: NavParams) { 
+  }
 
   ngOnInit() {
   }
 
+  public cerrarDirections() {
+    this.navParams.data.parentRef.cerrarDirections();
+  }
 }
