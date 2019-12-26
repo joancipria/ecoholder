ddac22fa6166435636cd5a28ed5b1ff2b3b37e1c
Joan Ciprià <joancipria@gmail.com>
2019-12-26 16:58:52 +0100

    Created navigation drawer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ import { Push, PushObject, PushOptions } from '@ionic-native/push/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  private navigate: any;
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
@@ -18,8 +19,9 @@ export class AppComponent {
     private push: Push
   ) {
     this.initializeApp();
+    this.sideMenu();
     this.hasPermission();
-    
+
   }
   initializeApp() {
     this.platform.ready().then(() => {
@@ -28,65 +30,81 @@ export class AppComponent {
     });
   }
 
-/**********************************************
-@description Crear canal del servicio de notificaciones push
-@design f() 
-@author Juan Andrés Canet Rodriguez
-@date 03/12/2019
-***********************************************/
-  hasPermission(){
-     //Notificaciones push
+  sideMenu() {
+    this.navigate =
+      [
+        {
+          title: "App",
+          url: "/app/tabs/home",
+          icon: "phone-portrait"
+        },
+        {
+          title: "Dashboard",
+          url: "/dashboard",
+          icon: "apps"
+        }
+      ]
+  }
+
+  /**********************************************
+  @description Crear canal del servicio de notificaciones push
+  @design f() 
+  @author Juan Andrés Canet Rodriguez
+  @date 03/12/2019
+  ***********************************************/
+  hasPermission() {
+    //Notificaciones push
     // comprobamos los permisos
     this.push.hasPermission()
-    .then((res: any) => {
-      if (res.isEnabled) {
-        this.push.createChannel({
-          id:"CanalTest",
-          description:"MiCanal",
-          importance: 5
-        }).then(()=> console.log('canal creado'))
-        this.pushOptions();
-        console.log('Tenemos permiso para enviar notificaciones');
-      } else {
-        console.log('No tenemos permiso para enviar notificaciones');
-      }
-    });
+      .then((res: any) => {
+        if (res.isEnabled) {
+          this.push.createChannel({
+            id: "CanalTest",
+            description: "MiCanal",
+            importance: 5
+          }).then(() => console.log('canal creado'))
+          this.pushOptions();
+          console.log('Tenemos permiso para enviar notificaciones');
+        } else {
+          console.log('No tenemos permiso para enviar notificaciones');
+        }
+      });
   }
-/**********************************************
-@description Datos de conexion de firebase message
-@design f() 
-@author Juan Andrés Canet Rodriguez
-@date 03/12/2019
-***********************************************/
-  pushOptions(){
+  /**********************************************
+  @description Datos de conexion de firebase message
+  @design f() 
+  @author Juan Andrés Canet Rodriguez
+  @date 03/12/2019
+  ***********************************************/
+  pushOptions() {
     // inicializamos la configuración para android y ios
     const options: PushOptions = {
       android: {
-          senderID: '838602587969'
+        senderID: '838602587969'
       },
       ios: {
-          alert: 'true',
-          badge: true,
-          sound: 'false'
+        alert: 'true',
+        badge: true,
+        sound: 'false'
       },
       windows: {},
       browser: {
-        pushServiceURL:"http://push.api.phonegap.com/v1/push"
+        pushServiceURL: "http://push.api.phonegap.com/v1/push"
       }
-      };
-      const pushObject: PushObject = this.push.init(options);
-      pushObject.on('notification').subscribe((notification: any) => {
+    };
+    const pushObject: PushObject = this.push.init(options);
+    pushObject.on('notification').subscribe((notification: any) => {
       //aquí recibimos las notificaciones de firebase
-      });
-      pushObject.on('registration').subscribe((registration: any) => {
-        const registrationId = registration.registrationId;
-      });
-      pushObject.on('error').subscribe(error => {
-        console.error('Error with Push plugin', error)
-      });
-      //FIn Notificaciones push
-    }
-  
-    
+    });
+    pushObject.on('registration').subscribe((registration: any) => {
+      const registrationId = registration.registrationId;
+    });
+    pushObject.on('error').subscribe(error => {
+      console.error('Error with Push plugin', error)
+    });
+    //FIn Notificaciones push
+  }
+
+
 }
 
